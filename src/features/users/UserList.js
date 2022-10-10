d404fae0d971753db75e51a7c58270909124b35f
ahmedbukhari <ahmedbukhari@savvy.com.pk>
2022-10-10 16:14:58 -0700

    .

--- src/features/users/UserList.js
+++ src/features/users/UserList.js
@@ -4,7 +4,17 @@ const UserList = () => {
   { id: '2', name: 'John', email: 'john@mail.com'}
 ];
 
-const renderCard = () => <div>Users here</div> 
+const renderCard = () => users.map(user => (
+  <div className="bg-gray-300 p-5 flex items-center justify-between">
+    <div>
+      <h3 className="font-bold text-lg text-gray-700">{user.name}</h3>
+      <span className="font-normal text-gray-600">{user.email}</span>
+    </div>
+    <div>
+
+    </div>
+  </div>
+))
 
 return (
   <div className="grid gap-5 md:grid-cols-2">
