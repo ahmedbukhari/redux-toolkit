7c57e144ee49591f93ae2f18e36829ceac3c1bd5
ahmedbukhari <ahmedbukhari@savvy.com.pk>
2022-10-10 16:14:53 -0700

    .

--- src/features/users/UserList.js
+++ src/features/users/UserList.js
@@ -1,9 +1,17 @@
-import React from 'react'
+const UserList = () => {
+  const users = [
+  { id: '1', name: 'Amanda', email: 'amanda@mail.com' },
+  { id: '2', name: 'John', email: 'john@mail.com'}
+];
 
-function UserList() {
-  return (
-    <div>UserList</div>
-  )
+const renderCard = () => <div>Users here</div> 
+
+return (
+  <div className="grid gap-5 md:grid-cols-2">
+  {users.length ? renderCard() : <p className="text-center 
+  col-span-2 text-gray-700 font-semibold">No User</p>}
+  </div>
+)
 }
 
 export default UserList
