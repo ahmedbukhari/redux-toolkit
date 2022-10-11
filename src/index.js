2854e121d3e38124be1fab971a6f76d9555e73e9
ahmedbukhari <ahmedbukhari@savvy.com.pk>
2022-10-11 14:16:03 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
+const container = document.getElementById('root');
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
